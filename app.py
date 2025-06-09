0b524d0b0eea7650ac95135db2af3684c9b05c6a
Abhishek Dubey <abhishekdubey666@gmail.com>
2025-06-09 14:06:13 +0530

    Added New option in the menu

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from PyQt5.QtWidgets import (
     QGridLayout, QWidget, QMessageBox, QScrollBar
 )
 
+# Constants for the new empty table
+INITIAL_NEW_ROWS = 50
+INITIAL_NEW_COLS = 20
+
 class CsvTableWidget(QTableWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -28,24 +32,55 @@ class CsvTableWidget(QTableWidget):
         self.setHorizontalScrollMode(QTableWidget.ScrollPerPixel)
         self.setVerticalScrollMode(QTableWidget.ScrollPerPixel)
 
+    def new_table(self):
+        """Create a new empty table with predefined dimensions."""
+        self.clear() # Clears everything including headers and items
+        self.setRowCount(INITIAL_NEW_ROWS)
+        self.setColumnCount(INITIAL_NEW_COLS)
+
+        # Set numbered headers
+        self.setHorizontalHeaderLabels([str(i + 1) for i in range(INITIAL_NEW_COLS)])
+        self.setVerticalHeaderLabels([str(i + 1) for i in range(INITIAL_NEW_ROWS)])
+
+        # Populate with empty, editable items
+        for r in range(INITIAL_NEW_ROWS):
+            for c in range(INITIAL_NEW_COLS):
+                item = QTableWidgetItem("") # Empty item
+                item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable)
+                accessible_name = f"cell_{r}_{c}"
+                item.setData(Qt.AccessibleTextRole, accessible_name)
+                self.setItem(r, c, item)
+
+
+    def clear_table_contents(self):
+        self.clear()
+        self.setRowCount(0)
+        self.setColumnCount(0)
+        self.setHorizontalHeaderLabels([])
+        self.setVerticalHeaderLabels([])
+
     def load_csv(self, file_path):
         with open(file_path, newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile)
             data = list(reader)
             if not data:
+                self.clear_table_contents() # Clear if CSV is empty
                 return
             self.setRowCount(len(data))
             self.setColumnCount(len(data[0]))
-            for row_idx, row in enumerate(data):
-                for col_idx, value in enumerate(row):
+
+            for row_idx, row_data in enumerate(data):
+                for col_idx, value in enumerate(row_data):
                     item = QTableWidgetItem(value)
-                    # Set unique accessible name and class for each cell
                     accessible_name = f"cell_{row_idx}_{col_idx}"
                     item.setData(Qt.AccessibleTextRole, accessible_name)
-                    item.setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-                    item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable)
-                    item.setWhatsThis("CsvTableCell")
+                    item.setFlags(item.flags() | Qt.ItemIsEditable) # Make cells editable
                     self.setItem(row_idx, col_idx, item)
+            
+            self.setHorizontalHeaderLabels([str(i+1) for i in range(self.columnCount())])
+            self.setVerticalHeaderLabels([str(i+1) for i in range(self.rowCount())])
+            self.resizeColumnsToContents() # Optional: adjust column widths
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -115,6 +150,12 @@ class MainWindow(QMainWindow):
 
         # File Menu
         file_menu = menubar.addMenu("&File")
+
+        new_action = QAction("&New", self)
+        new_action.triggered.connect(lambda: self.table.clearContents())
+        new_action.triggered.connect(self.new_table)
+        file_menu.addAction(new_action)
+
         open_action = QAction("&Open CSV...", self)
         open_action.triggered.connect(self.open_csv)
         file_menu.addAction(open_action)
@@ -144,6 +185,10 @@ class MainWindow(QMainWindow):
         about_action = QAction("&About", self)
         about_action.triggered.connect(self.show_about)
         help_menu.addAction(about_action)
+
+    def new_table(self):
+        self.table.new_table()
+        self.statusBar().showMessage("New table created with default size.")
 
     def open_csv(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv)")
